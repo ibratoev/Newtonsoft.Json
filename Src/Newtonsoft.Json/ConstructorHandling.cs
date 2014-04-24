24d5cbd44671b08ac656e2296682b566a17407ba
Ivaylo Bratoev <ivaylo.bratoev@gmail.com>
2014-04-24 17:47:54 +0300

    test

--- Src/Newtonsoft.Json/ConstructorHandling.cs
+++ Src/Newtonsoft.Json/ConstructorHandling.cs
@@ -23,7 +23,7 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 #endregion
 
-namespace Newtonsoft.Json
+1namespace Newtonsoft.Json
 {
     /// <summary>
     /// Specifies how constructors are used when initializing objects during deserialization by the <see cref="JsonSerializer"/>.
